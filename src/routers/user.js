64bb965814d7f4ffb954c3446cda6c7ffae8c54a
DANIELlux1 <daniellux1moreira@gmail.com>
2019-11-30 14:39:40 +0100

    State after course part 109

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const router = new express.Router()
+const auth = require("../middleware/auth")
 const User = require("../models/user")
 
 router.post("/users",async (req, res) => {
@@ -31,14 +32,9 @@ router.post("/users/login", async (req,res) => {
     }
 })
 
-router.get("/users",async (req,res) => {
+router.get("/users/me", auth, async (req,res) => {
     
-    try{
-        const users = await User.find({})
-        res.send(users)
-    }catch{
-        res.status(500).send
-    }
+    res.send(req.user)
 })
 
 router.get("/users/:id", async (req,res) => {
